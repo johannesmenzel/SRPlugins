d080cf6f2abbf96cc8dbf7379fd8fdce4d9351b7
johannesmenzel <johannes.menzel@posteo.de>
2021-12-04 15:09:55 +0100

    init configuration

--- Plugins/SRDynamicsControl/config.h
+++ Plugins/SRDynamicsControl/config.h
@@ -1,12 +1,12 @@
 #define PLUG_NAME "SRDynamicsControl"
 #define PLUG_MFR "SRPlugins"
-#define PLUG_VERSION_HEX 0x00010000
-#define PLUG_VERSION_STR "1.0.0"
-#define PLUG_UNIQUE_ID 'i3Gt'
-#define PLUG_MFR_ID 'Acme'
-#define PLUG_URL_STR "https://iplug2.github.io"
+#define PLUG_VERSION_HEX 0x00000001
+#define PLUG_VERSION_STR "0.0.1"
+#define PLUG_UNIQUE_ID 'SrDC'
+#define PLUG_MFR_ID 'SrPl'
+#define PLUG_URL_STR "https://github.com/johannesmenzel/SRPlugins"
 #define PLUG_EMAIL_STR "spam@me.com"
-#define PLUG_COPYRIGHT_STR "Copyright 2020 Acme Inc"
+#define PLUG_COPYRIGHT_STR "Copyright 2021 Johannes Menzel"
 #define PLUG_CLASS_NAME SRDynamicsControl
 
 #define BUNDLE_NAME "SRDynamicsControl"
@@ -24,7 +24,7 @@
 #define PLUG_DOES_MPE 0
 #define PLUG_DOES_STATE_CHUNKS 0
 #define PLUG_HAS_UI 1
-#define PLUG_WIDTH 600
+#define PLUG_WIDTH 800
 #define PLUG_HEIGHT 600
 #define PLUG_FPS 60
 #define PLUG_SHARED_RESOURCES 0
