0e203b9f194df80ed73bc54fd9fbc94caf182697
johannesmenzel <johannes.menzel@posteo.de>
2021-12-05 15:56:48 +0100

    Working on Compressor, added old code

--- Plugins/SRDynamicsControl/SRDynamicsControl.cpp
+++ Plugins/SRDynamicsControl/SRDynamicsControl.cpp
@@ -2,56 +2,171 @@
 #include "IPlug_include_in_plug_src.h"
 #include "IControls.h"
 #include "../../Classes/Graphics/SRCustomKnob.h"
+#include "../../Classes/Graphics/SRCustomLayout.h"
 
 SRDynamicsControl::SRDynamicsControl(const InstanceInfo& info)
-: Plugin(info, MakeConfig(kNumParams, kNumPresets))
-, fOutGain(100)
+	: Plugin(info, MakeConfig(kNumParams, kNumPresets))
+	, fOutGain(100)
+	, fCompLevel()
+	, fCompOpto()
+	, fDeesser()
+	, fCompFet()
+	, fCompLim()
 {
-  GetParam(kGain)->InitDouble("Gain", 0., -12., 12.0, 0.01, "dB");
-  GetParam(kThreshLevel)->InitDouble("Thresh Level", 0., -30., 0., 0.01, "dB");
-  GetParam(kThreshTrans)->InitDouble("Thresh Trans", 0., -30., 0.0, 0.01, "dB");
+	GetParam(kGain)->InitDouble("Gain", 0., -12., 12.0, 0.01, "dB");
+	GetParam(kThresh)->InitDouble("Thresh", 0., 0., 100., 0.01, "%");
+	GetParam(kCrest)->InitDouble("Crest", 50., 0., 100., 0.01, "%");
+	GetParam(kRatio)->InitDouble("Ratio", 50., 0., 100., 0.01, "%");
+	GetParam(kAttack)->InitDouble("Attack", 50., 0., 100., 0.01, "%");
+	GetParam(kRelease)->InitDouble("Release", 50., 0., 100., 0.01, "%");
+	SetCompressorValues();
+
 
 #if IPLUG_EDITOR // http://bit.ly/2S64BDd
-  mMakeGraphicsFunc = [&]() {
-    return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
-  };
-  
-  mLayoutFunc = [&](IGraphics* pGraphics) {
-    pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
-    pGraphics->AttachPanelBackground(COLOR_GRAY);
-    pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
-    const IRECT b = pGraphics->GetBounds();
-    //pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "SRDynamicsControl", IText(50)));
-    //pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
-    pGraphics->AttachControl(new Knob(b.GetGridCell(1, 0, 2, 3), kGain));
-    pGraphics->AttachControl(new Knob(b.GetGridCell(1, 1, 2, 3), kThreshLevel));
-    pGraphics->AttachControl(new Knob(b.GetGridCell(1, 2, 2, 3), kThreshTrans));
-  };
+	mMakeGraphicsFunc = [&]() {
+		return MakeGraphics(*this, PLUG_WIDTH, PLUG_HEIGHT, PLUG_FPS, GetScaleForScreen(PLUG_WIDTH, PLUG_HEIGHT));
+	};
+
+	mLayoutFunc = [&](IGraphics* pGraphics) {
+		pGraphics->AttachCornerResizer(EUIResizerMode::Scale, false);
+		pGraphics->AttachPanelBackground(SR::Graphics::Layout::SR_DEFAULT_COLOR_CUSTOM_PANEL_BG);
+		pGraphics->LoadFont("Roboto-Regular", ROBOTO_FN);
+		const IRECT b = pGraphics->GetBounds();
+		//pGraphics->AttachControl(new ITextControl(b.GetMidVPadded(50), "SRDynamicsControl", IText(50)));
+		//pGraphics->AttachControl(new IVKnobControl(b.GetCentredInside(100).GetVShifted(-100), kGain));
+		pGraphics->AttachControl(new SR::Graphics::Controls::Knob(b.GetGridCell(0, 0, 2, 4), kAttack, "Attack", SR::Graphics::Layout::SR_DEFAULT_STYLE, true, false, -150.f, 150.f, -150.f, EDirection::Vertical, 4., 1.f), cAttack);
+		pGraphics->AttachControl(new SR::Graphics::Controls::Knob(b.GetGridCell(0, 1, 2, 4), kRelease, "Release", SR::Graphics::Layout::SR_DEFAULT_STYLE, true, false, -150.f, 150.f, -150.f, EDirection::Vertical, 4., 1.f), cRelease);
+		pGraphics->AttachControl(new SR::Graphics::Controls::Knob(b.GetGridCell(0, 2, 2, 4), kCrest, "Crest", SR::Graphics::Layout::SR_DEFAULT_STYLE, true, false, -150.f, 150.f, -150.f, EDirection::Vertical, 4., 1.f), cCrest);
+		pGraphics->AttachControl(new SR::Graphics::Controls::Knob(b.GetGridCell(1, 0, 2, 4), kThresh, "Tresh", SR::Graphics::Layout::SR_DEFAULT_STYLE, true, false, -150.f, 150.f, -150.f, EDirection::Vertical, 4., 1.f), cThresh);
+		pGraphics->AttachControl(new SR::Graphics::Controls::Knob(b.GetGridCell(1, 1, 2, 4), kRatio, "Ratio", SR::Graphics::Layout::SR_DEFAULT_STYLE, true, false, -150.f, 150.f, -150.f, EDirection::Vertical, 4., 1.f), cRatio);
+		pGraphics->AttachControl(new SR::Graphics::Controls::Knob(b.GetGridCell(1, 2, 2, 4), kGain, "Out Gain", SR::Graphics::Layout::SR_DEFAULT_STYLE, true, false, -150.f, 150.f, -150.f, EDirection::Vertical, 4., 1.f), cGain);
+		pGraphics->AttachControl(new IVMeterControl<2>(b.GetGridCell(0, 14, 1, 16), "In", SR::Graphics::Layout::SR_DEFAULT_STYLE, EDirection::Vertical, { "L", "R" }, 0, -60.f, 0.f), cMeterIn);
+		pGraphics->AttachControl(new IVMeterControl<2>(b.GetGridCell(0, 15, 1, 16), "Out", SR::Graphics::Layout::SR_DEFAULT_STYLE, EDirection::Vertical, { "L", "R" }, 0, -60.f, 0.f), cMeterOut);
+		pGraphics->AttachControl(new IVMeterControl<1>(b.GetGridCell(0, 24, 1, 32), "", SR::Graphics::Layout::SR_DEFAULT_STYLE, EDirection::Vertical, { }, 0, -20.f, 0.f), cMeterGrLevel);
+		pGraphics->AttachControl(new IVMeterControl<1>(b.GetGridCell(0, 25, 1, 32), "", SR::Graphics::Layout::SR_DEFAULT_STYLE, EDirection::Vertical, { }, 0, -10.f, 0.f), cMeterGrOpto);
+		pGraphics->AttachControl(new IVMeterControl<1>(b.GetGridCell(0, 26, 1, 32), "", SR::Graphics::Layout::SR_DEFAULT_STYLE, EDirection::Vertical, { }, 0, -5.f, 0.f), cMeterGrFet);
+		pGraphics->AttachControl(new IVMeterControl<1>(b.GetGridCell(0, 27, 1, 32), "", SR::Graphics::Layout::SR_DEFAULT_STYLE, EDirection::Vertical, { }, 0, -2.f, 0.f), cMeterGrLim);
+	};
 #endif
 }
 
 #if IPLUG_DSP
-void SRDynamicsControl::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+void SRDynamicsControl::OnIdle()
 {
-  const double gain = DBToAmp(GetParam(kGain)->Value());
-  const int nChans = NOutChansConnected();
-  
-  for (int s = 0; s < nFrames; s++) {
-    for (int c = 0; c < nChans; c++) {
-      outputs[c][s] = inputs[c][s];
-      fOutGain.Process(outputs[c][s], outputs[c][s]);
-    }
-  }
+	mMeterSenderIn.TransmitData(*this);
+	mMeterSenderOut.TransmitData(*this);
+	mMeterSenderGrLevel.TransmitData(*this);
+	mMeterSenderGrOpto.TransmitData(*this);
+	mMeterSenderGrFet.TransmitData(*this);
+	mMeterSenderGrLim.TransmitData(*this);
+}
+void SRDynamicsControl::SetCompressorValues()
+{
+	const double samplerate = GetSampleRate();
+	const double thresh = GetParam(kThresh)->Value() * -.01;
+	const double crest = GetParam(kCrest)->Value() * .02;
+	const double ratio = GetParam(kRatio)->Value() * .01;
+	const double attack = GetParam(kAttack)->Value() * .01;
+	const double release = GetParam(kRelease)->Value() * .01;
+
+	fCompLevel.ResetCompressor(
+		thresh * 48. * (2. - crest), // thresh
+		1. / (1. + ratio * 1.), // ratio
+		20. + attack * 80., // attack
+		500. + release * 4500., // release
+		.0, // sidechain HP
+		10., // knee dB
+		false, // feedback
+		true, // automake
+		-18., // reference
+		samplerate);
+
+	fCompOpto.ResetCompressor(
+		thresh * 36. * (1.5 - crest * .5), // thresh
+		1. / (1. + ratio * 4.), // ratio
+		5. + attack * 20., // attack
+		200. + release * 1800., // release
+		.0, // sidechain HP
+		6., // knee dB
+		false, // feedback
+		true, // automake
+		-18., // reference
+		samplerate);
+
+	fCompFet.ResetCompressor(
+		thresh * 24. * (.5 + crest * .5), // thresh
+		1. / (1. + ratio * 19.), // ratio
+		1. + attack * 4., // attack
+		40. + release * 360., // release
+		.0, // sidechain HP
+		4., // knee dB
+		false, // feedback
+		true, // automake
+		-18., // reference
+		samplerate);
+
+	fCompLim.ResetCompressor(
+		thresh * 12. * crest, // thresh
+		1. / (1. + ratio * 99.), // ratio
+		.02 + attack * .08, // attack
+		20. + release * 180., // release
+		.0, // sidechain HP
+		2., // knee dB
+		false, // feedback
+		true, // automake
+		-18., // reference
+		samplerate);
+	
 }
 
-//void SRDynamicsControl::OnReset() {
-//    fOutGain.InitGain(100);
-//}
+void SRDynamicsControl::ProcessBlock(sample** inputs, sample** outputs, int nFrames)
+{
+	const int nChans = NOutChansConnected();
+
+	for (int s = 0; s < nFrames; s++) {
+		for (int c = 0; c < nChans; c++) {
+			outputs[c][s] = inputs[c][s];
+		}
+
+		fCompLevel.Process(outputs[0][s], outputs[1][s]);
+		fCompOpto.Process(outputs[0][s], outputs[1][s]);
+		fCompFet.Process(outputs[0][s], outputs[1][s]);
+		fCompLim.Process(outputs[0][s], outputs[1][s]);
+
+		for (int c = 0; c < nChans; c++) {
+			fOutGain.Process(outputs[c][s], outputs[c][s]);
+		}
+
+		mBufferMeterGrLevel.ProcessBuffer(fCompLevel.GetGrLin(), 0, s);
+		mBufferMeterGrOpto.ProcessBuffer(fCompOpto.GetGrLin(), 0, s);
+		mBufferMeterGrFet.ProcessBuffer(fCompFet.GetGrLin(), 0, s);
+		mBufferMeterGrLim.ProcessBuffer(fCompLim.GetGrLin(), 0, s);
+
+	}
+
+	mMeterSenderIn.ProcessBlock(inputs, nFrames, cMeterIn);
+	mMeterSenderOut.ProcessBlock(outputs, nFrames, cMeterOut);
+	mMeterSenderGrLevel.ProcessBlock(mBufferMeterGrLevel.GetBuffer(), nFrames, cMeterGrLevel);
+	mMeterSenderGrOpto.ProcessBlock(mBufferMeterGrOpto.GetBuffer(), nFrames, cMeterGrOpto);
+	mMeterSenderGrFet.ProcessBlock(mBufferMeterGrFet.GetBuffer(), nFrames, cMeterGrFet);
+	mMeterSenderGrLim.ProcessBlock(mBufferMeterGrLim.GetBuffer(), nFrames, cMeterGrLim);
+}
+
+void SRDynamicsControl::OnReset() {
+
+}
+
 void SRDynamicsControl::OnParamChange(int paramIdx)
 {
-    switch (paramIdx)
-    {
-    case kGain: fOutGain.SetGain(DBToAmp(GetParam(paramIdx)->Value())); break;
-    }
+	switch (paramIdx)
+	{
+	case kGain: fOutGain.SetGain(DBToAmp(GetParam(paramIdx)->Value())); break;
+	case kThresh:
+	case kRatio:
+	case kAttack:
+	case kRelease:
+	case kCrest:
+		SetCompressorValues(); break;
+	}
 }
 #endif
--- Plugins/SRDynamicsControl/SRDynamicsControl.h
+++ Plugins/SRDynamicsControl/SRDynamicsControl.h
@@ -2,15 +2,38 @@
 
 #include "IPlug_include_in_plug_hdr.h"
 #include "../../Classes/DSP/SRGain.h"
+#include "../../Classes/DSP/SRDynamics.h"
+#include "../../Classes/DSP/SRBuffer.h"
+#include "IControls.h"
 
 const int kNumPresets = 1;
 
 enum EParams
 {
-  kGain = 0,
-  kThreshLevel,
-  kThreshTrans,
-  kNumParams
+	kGain = 0,
+	kThresh,
+	kCrest,
+	kRatio,
+	kAttack,
+	kRelease,
+	kNumParams
+};
+
+enum ECtrlTags
+{
+	cGain = 0,
+	cThresh,
+	cCrest,
+	cRatio,
+	cAttack,
+	cRelease,
+	cMeterIn,
+	cMeterOut,
+	cMeterGrLevel,
+	cMeterGrOpto,
+	cMeterGrFet,
+	cMeterGrLim,
+	kNumCtrlTags
 };
 
 using namespace iplug;
@@ -19,13 +42,31 @@ using namespace igraphics;
 class SRDynamicsControl final : public Plugin
 {
 public:
-  SRDynamicsControl(const InstanceInfo& info);
+	SRDynamicsControl(const InstanceInfo& info);
 
 #if IPLUG_DSP // http://bit.ly/2S64BDd
-  void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
-  //void OnReset() override;
-  void OnParamChange(int paramIdx) override;
-#endif
+	void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
+	void SetCompressorValues();
+	void OnReset() override;
+	void OnIdle() override;
+	void OnParamChange(int paramIdx) override;
 private:
 	SR::DSP::SRGain fOutGain;
+	SR::DSP::SRCompressor fCompLevel;
+	SR::DSP::SRCompressor fCompOpto;
+	SR::DSP::SRDeesser fDeesser;
+	SR::DSP::SRCompressor fCompFet;
+	SR::DSP::SRCompressor fCompLim;
+	//SR::DSP::SRBuffer<sample, 4, 1024> mBufferGr;
+	IPeakSender<2> mMeterSenderIn;
+	IPeakSender<2> mMeterSenderOut;
+	IPeakSender<1> mMeterSenderGrLevel;
+	IPeakSender<1> mMeterSenderGrOpto;
+	IPeakSender<1> mMeterSenderGrFet;
+	IPeakSender<1> mMeterSenderGrLim;
+	SR::DSP::SRBuffer<sample, 1, 1024> mBufferMeterGrLevel;
+	SR::DSP::SRBuffer<sample, 1, 1024> mBufferMeterGrOpto;
+	SR::DSP::SRBuffer<sample, 1, 1024> mBufferMeterGrFet;
+	SR::DSP::SRBuffer<sample, 1, 1024> mBufferMeterGrLim;
+#endif
 };
