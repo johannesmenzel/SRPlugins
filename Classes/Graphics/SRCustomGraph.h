9535aad8937422110de0c9d0baa8a33c0eebbd6f
Johannes Menzel <johannes.menzel@posteo.de>
2023-01-21 18:53:32 +0100

    Prevent graph control from drawing values equaling or exceeding minimum or maximum

--- Classes/Graphics/SRCustomGraph.h
+++ Classes/Graphics/SRCustomGraph.h
@@ -11,136 +11,137 @@ namespace SR {
 	namespace Graphics {
 		namespace Controls {
 
-            // TODO: Draw with PathConvexShape from ptr to member array updated from Updatefunction
-            class SRGraphBase
-                : public IControl
-                , public IVectorBase
-            {
-            public:
-                SRGraphBase(IRECT bounds, int numValues, float* values, const IVStyle& style = DEFAULT_STYLE)
-                    : IControl(bounds, -1)
-                    , IVectorBase(style)
-                    , mValues(values)
-                    , mNumValues(numValues)
-                    , mX(new float[numValues])
-                    , mY(new float[numValues])
-                {
-                    AttachIControl(this, ""); // TODO: Should hand label
-                }
+			// TODO: Draw with PathConvexShape from ptr to member array updated from Updatefunction
+			class SRGraphBase
+				: public IControl
+				, public IVectorBase
+			{
+			public:
+				SRGraphBase(IRECT bounds, int numValues, float* values, const IVStyle& style = DEFAULT_STYLE)
+					: IControl(bounds, -1)
+					, IVectorBase(style)
+					, mValues(values)
+					, mNumValues(numValues)
+					, mX(new float[numValues])
+					, mY(new float[numValues])
+				{
+					AttachIControl(this, ""); // TODO: Should hand label
+				}
 
-                ~SRGraphBase() {
-                    //delete[] mX;
-                    //delete[] mY;
-                    //delete[] mValues;
-                }
+				~SRGraphBase() {
+					//delete[] mX;
+					//delete[] mY;
+					//delete[] mValues;
+				}
 
-                void Draw(IGraphics& g) override {
-                    // Fill Graph
-                    g.PathClear();
-                    g.PathMoveTo(mRECT.L, mRECT.MH());
-                    for (int i = 0; i < mNumValues; i++) {
-                        g.PathLineTo(mX[i], mY[i]);
-                    }
-                    g.PathLineTo(mRECT.R, mRECT.MH());
-                    g.PathClose();
-                    g.PathFill(GetColor(kHL));
+				void Draw(IGraphics& g) override {
+					// Fill Graph
+					g.PathClear();
+					g.PathMoveTo(mRECT.L, mRECT.MH());
+					for (int i = 0; i < mNumValues; i++) {
+						g.PathLineTo(mX[i], mY[i]);
+					}
+					g.PathLineTo(mRECT.R, mRECT.MH());
+					g.PathClose();
+					g.PathFill(GetColor(kHL));
 
-                    // Draw Graph
-                    g.PathClear();
-                    g.PathMoveTo(mRECT.L, mRECT.MH());
-                    for (int i = 0; i < mNumValues; i++) {
-                        //if (mY[i] == mRECT.T && mY[i-1] == mRECT.T || mY[i] == mRECT.B && mY[i - 1] == mRECT.B)
-                        //  g.PathMoveTo(mX[i], mY[i]);
-                        //else
-                        g.PathLineTo(mX[i], mY[i]);
-                    }
-                    g.PathLineTo(mRECT.R, mRECT.MH());
-                    g.PathStroke(GetColor(kPR), mStyle.frameThickness);
-                };
+					// Draw Graph
+					g.PathClear();
+					g.PathMoveTo(mX[0], mY[0]);
+					for (int i = 1; i < mNumValues; i++) {
+						if (mY[i] >= mRECT.B || mY[i] <= mRECT.T) {
+							g.PathMoveTo(mX[i], mY[i]);
+						}
+						else {
+							g.PathLineTo(mX[i], mY[i]);
+						}
+					}
+					g.PathStroke(GetColor(kFG), mStyle.frameThickness);
+				};
 
-                void OnResize() override {
-                    for (int i = 0; i < mNumValues; i++) {
-                        mX[i] = mRECT.L + (mRECT.W() / (mNumValues - 1.f)) * float(i);
-                        mY[i] = mRECT.MH() - (mRECT.H() * (float)mValues[i] * 0.5f);
-                    }
-                    SetDirty(false);
-                }
+				void OnResize() override {
+					for (int i = 0; i < mNumValues; i++) {
+						mX[i] = mRECT.L + (mRECT.W() / (mNumValues - 1.f)) * float(i);
+						mY[i] = mRECT.MH() - (mRECT.H() * (float)mValues[i] * 0.5f);
+					}
+					SetDirty(false);
+				}
 
-                void Process(float* values) {
-                    mValues = values;
-                    for (int i = 0; i < mNumValues; i++) {
-                        mValues[i] = Clip<float>(mValues[i], -1.f, 1.f);
-                    }
-                    OnResize();
-                };
-                //void OnMouseDown(float x, float y, const IMouseMod& mod) override;
-            private:
-                //WDL_String mDisp;
-                float* mValues;
-                int mNumValues;
-                float* mX;
-                float* mY;
-            };
+				void Process(float* values) {
+					mValues = values;
+					for (int i = 0; i < mNumValues; i++) {
+						mValues[i] = Clip<float>(mValues[i], -1.f, 1.f);
+					}
+					OnResize();
+				};
+				//void OnMouseDown(float x, float y, const IMouseMod& mod) override;
+			private:
+				//WDL_String mDisp;
+				float* mValues;
+				int mNumValues;
+				float* mX;
+				float* mY;
+			};
 
 
-            // TODO: Draw with PathConvexShape from ptr to member array updated from Updatefunction
-            class SRFrequencyResponseMeter
-                : public IControl
-                , public IVectorBase
-            {
-            public:
-                SRFrequencyResponseMeter(IRECT bounds, int numValues, double* values, double shape = 1.0, const IVStyle& style = DEFAULT_STYLE)
-                    : IControl(bounds, -1)
-                    , IVectorBase(style)
-                    , mValues(values)
-                    , mNumValues(numValues)
-                    , mShape(shape)
-                    , mPatternFill(IPattern(EPatternType::Solid))
-                    , mPatternStroke(IPattern(EPatternType::Solid))
-                {
-                    mStrokeOptions.mPreserve = true;
-                    mFillOptions.mPreserve = true;
-                    mPatternStroke = IPattern(GetColor(kFG));
-                    mPatternFill = IPattern(GetColor(kHL));
-                    AttachIControl(this, ""); // TODO: shoud hand label
-                }
+			// TODO: Draw with PathConvexShape from ptr to member array updated from Updatefunction
+			class SRFrequencyResponseMeter
+				: public IControl
+				, public IVectorBase
+			{
+			public:
+				SRFrequencyResponseMeter(IRECT bounds, int numValues, double* values, double shape = 1.0, const IVStyle& style = DEFAULT_STYLE)
+					: IControl(bounds, -1)
+					, IVectorBase(style)
+					, mValues(values)
+					, mNumValues(numValues)
+					, mShape(shape)
+					, mPatternFill(IPattern(EPatternType::Solid))
+					, mPatternStroke(IPattern(EPatternType::Solid))
+				{
+					mStrokeOptions.mPreserve = true;
+					mFillOptions.mPreserve = true;
+					mPatternStroke = IPattern(GetColor(kFG));
+					mPatternFill = IPattern(GetColor(kHL));
+					AttachIControl(this, ""); // TODO: shoud hand label
+				}
 
-                ~SRFrequencyResponseMeter() {}
+				~SRFrequencyResponseMeter() {}
 
-                void Draw(IGraphics& g) override {
-                    g.PathClear();
-                    g.PathMoveTo(mRECT.L, mRECT.MH());
+				void Draw(IGraphics& g) override {
+					g.PathClear();
+					g.PathMoveTo(mRECT.L, mRECT.MH());
 
-                    for (int i = 0; i < mNumValues; i++) {
-                        const float y = mRECT.MH() - ((float)mValues[i] * 0.5f * mRECT.H());
-                        const float x = mRECT.L + ((float)i / ((float)mNumValues - 1.f)) * mRECT.W();
-                        g.PathLineTo(x, y);
-                    }
+					for (int i = 0; i < mNumValues; i++) {
+						const float y = mRECT.MH() - ((float)mValues[i] * 0.5f * mRECT.H());
+						const float x = mRECT.L + ((float)i / ((float)mNumValues - 1.f)) * mRECT.W();
+						g.PathLineTo(x, y);
+					}
 
-                    g.PathLineTo(mRECT.R, mRECT.MH());
-                    //g.PathClose();
-                    g.PathFill(mPatternFill, mFillOptions, 0);
-                    g.PathStroke(mPatternStroke, 1.f, mStrokeOptions, 0);
-                };
+					g.PathLineTo(mRECT.R, mRECT.MH());
+					//g.PathClose();
+					g.PathFill(mPatternFill, mFillOptions, 0);
+					g.PathStroke(mPatternStroke, 1.f, mStrokeOptions, 0);
+				};
 
-                void Process(double* values) {
-                    mValues = values;
-                    for (int i = 0; i < mNumValues; i++) {
-                        mValues[i] = Clip<double>(mValues[i], -1, 1);
-                    }
-                    SetDirty(false);
-                };
-                //void OnMouseDown(float x, float y, const IMouseMod& mod) override;
-            private:
-                //WDL_String mDisp;
-                double* mValues;
-                int mNumValues;
-                double mShape;
-                IStrokeOptions mStrokeOptions;
-                IFillOptions mFillOptions;
-                IPattern mPatternFill;
-                IPattern mPatternStroke;
-            };
+				void Process(double* values) {
+					mValues = values;
+					for (int i = 0; i < mNumValues; i++) {
+						mValues[i] = Clip<double>(mValues[i], -1.f, 1.f);
+					}
+					SetDirty(false);
+				};
+				//void OnMouseDown(float x, float y, const IMouseMod& mod) override;
+			private:
+				//WDL_String mDisp;
+				double* mValues;
+				int mNumValues;
+				double mShape;
+				IStrokeOptions mStrokeOptions;
+				IFillOptions mFillOptions;
+				IPattern mPatternFill;
+				IPattern mPatternStroke;
+			};
 
 		}
 	}
